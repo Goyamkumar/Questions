4a527542092e1542ad47b0f57858710455d3faaf
GoyamNitroSlayer <98691410+GoyamNitroSlayer@users.noreply.github.com>
2022-10-02 23:49:16 +0530

    deleted

--- Numbers_with_same_consecutive_Differences.py
+++
@@ -1,5 +0,0 @@
-# 	967. Numbers With Same Consecutive Differences
-# 	Topics Related : Backtracking ,BFS
-#   Return : all non negative integers of length 'n' such 
-#   that that the absolute difference between two consecutive
-#   digits is k
